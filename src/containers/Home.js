0c90d384c966ad65549b81ce7e18dfeb7afbb044
Nirmata <labelkingz707@gmail.com>
2024-08-09 23:43:40 +1200

    Initial project structure setup

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+// containers/Home.js
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Crypto Wallet</h1>
+    {/* Add content and components */}
+  </div>
+);
+
+export default Home;
---
+++ src/containers/Wallet.js
@@ -0,0 +1,11 @@
+// containers/Wallet.js
+import React from 'react';
+
+const Wallet = () => (
+  <div>
+    <h1>Your Wallet</h1>
+    {/* Add wallet balance and transaction components */}
+  </div>
+);
+
+export default Wallet;
