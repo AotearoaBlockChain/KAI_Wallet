bbd3e38aa42a6d299e2149118d2ba157be36af71
Nirmata <labelkingz707@gmail.com>
2024-08-10 01:47:14 +1200

    update package.json

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // This import is correct
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to KAI_Wallet</h1>
+    </div>
+  );
+}
+
+export default App;
